ec155a984bb9c13ab8d28fd8b3a867ca6b063718
Daniel <debem1972@gmail.com>
2023-08-07 16:30:17 -0300

    Foram criados as primeiras linhas de código do sass;Foi criado o doc _variaveis.scss

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 function anota() {
-    let anotar = document.getElementById('entrada').value;  //Recebe os dados digitados no input
-    let ulList = document.getElementById('lista').innerHTML; //Rece os dados carregados na ul/li
+    let text = document.getElementById('entrada').value;  //Recebe os dados digitados no input
+    let listaDeDados = document.getElementById('lista').innerHTML; //Recebe os dados carregados na ul/li
 
-    ulList += '<li>' + anotar + '</li>'  //Concatena cada tag li de entrada e saída ao conteúdo digitado no input e salvo na variável anotar
+    listaDeDados += '<li>' + text + '</li>'  //Concatena cada tag li de abertura e fechamento ao conteúdo digitado no input e salvo na variável text
 
 
-    //Verifica se o input está vazio. Se sim, emite aviso, se não, envia para o id lista o conteúdo da variável ulList
+    //Verifica se o input está vazio. Se sim, emite aviso, se não, envia para o id lista o conteúdo da variável listaDeDados
     if (document.getElementById('entrada').value == "") {
         window.alert("Escreva algo!");
     } else {
-        document.getElementById('lista').innerHTML = ulList;
+        document.getElementById('lista').innerHTML = listaDeDados;
     }
 
-    document.getElementById('entrada').value = null;  //Limpa o campo do input após a function anotar
+    document.getElementById('entrada').value = null;  //Limpa o campo do input após a function anota
 
 }
 
@@ -20,7 +20,7 @@ function anota() {
 
 //Salva a lista no localStorage após clicar no botão salvar
 function salva() {
-    const meusDados = document.querySelector('#ulList').innerHTML
+    const meusDados = document.querySelector('#lista').innerHTML;
     localStorage.setItem('meusDados', meusDados);
     window.alert('Sua lista foi salva!!!');
 
@@ -28,7 +28,8 @@ function salva() {
 
 //-------------*-----------------------*------------------------------------
 
+//Busca a lista salva no localStorage pelo método getItem com a chave meusDados, e a envia para a ul id lista.
 function busca() {
     const meusDados = localStorage.getItem('meusDados');
-    document.querySelector('#ulList').innerHTML = meusDados;
+    document.querySelector('#lista').innerHTML = meusDados;
 }
